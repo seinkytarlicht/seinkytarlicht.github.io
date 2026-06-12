7dbfe48e7ae4fccdb033d2702aa86482bfeccb24
Seinkyt Arlicht <150875749+seinkytarlicht@users.noreply.github.com>
2026-06-13 02:07:39 +0700

    google-site-verification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      meta: [
+        {
+          name: "google-site-verification",
+          content: "OIM5c_P0ZbCLmgtNpbryqwvsEqpPSAY9enx7FpGdVf8",
+        },
+      ],
+    },
+  },
 });
