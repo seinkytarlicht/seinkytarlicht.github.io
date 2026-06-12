6337b4d00abf5f93cc163c10c865a018a270134c
Seinkyt Arlicht <150875749+seinkytarlicht@users.noreply.github.com>
2026-06-12 21:23:51 +0700

    Sigh....

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,52 @@
+<script setup lang="ts">
+useHead({
+  title: "Seinkyt Arlicht Website",
+  meta: [
+    { name: "description", content: "Nothing in here, just blank website" },
+    { property: "og:title", content: "Seinkyt Arlicht Website" },
+    {
+      property: "og:description",
+      content: "Nothing in here, just blank website",
+    },
+  ],
+});
+
+const whatever = ref([
+  {
+    name: "Shaula Gesugao",
+    image: "/images/shaula.png",
+  },
+  {
+    name: "Meili Portroute Gesugao",
+    image: "/images/meili.png",
+  },
+  {
+    name: "Eridona Gesugao",
+    image: "/images/eridona.png",
+  },
+  {
+    name: "Ram Gesugao",
+    image: "/images/ram.png",
+  },
+  {
+    name: "Julius Juukulius Gesugao",
+    image: "/images/julius.png",
+  },
+  {
+    name: "Beatrice Gesugao",
+    image: "/images/beatrice.png",
+  },
+  {
+    name: "Emilia Gesugao",
+    image: "/images/emilia.png",
+  },
+]);
+</script>
+
 <template>
-  <h1>Hello World</h1>
+  <h1>Here some screnshoot from new eps of rezero</h1>
+
+  <div class="flex flex-wrap gap-4">
+    <img :src="w.image" :alt="w.name" v-for="w in whatever" width="480" />
+  </div>
 </template>
---
+++ app/pages/remusika.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+useHead({
+  title: "Seinkyt Arlicht - ReMusika",
+  meta: [
+    {
+      name: "description",
+      content:
+        "ReMusika is a lightweight local music player powered by Go and Nuxt. All music playback happens locally on your device. No internet connection, external services, or additional setup required. Simply run the application, open your browser, and start listening to your music library.",
+    },
+    { property: "og:title", content: "Seinkyt Arlicht - ReMusika" },
+    {
+      property: "og:description",
+      content:
+        "ReMusika is a lightweight local music player powered by Go and Nuxt. All music playback happens locally on your device. No internet connection, external services, or additional setup required. Simply run the application, open your browser, and start listening to your music library.",
+    },
+  ],
+});
+</script>
+
+<template>
+  <UContainer class="flex flex-col gap-3">
+    <h2 class="text-4xl">ReMusika</h2>
+    <p>
+      ReMusika is a lightweight local music player powered by Go and Nuxt. All
+      music playback happens locally on your device.
+    </p>
+    <p>
+      No internet connection, external services, or additional setup required.
+      Simply run the application, open your browser, and start listening to your
+      music library.
+    </p>
+
+    <h3 class="text-3xl">Download</h3>
+    <a
+      href="https://github.com/seinkytarlicht/remusika/releases/latest"
+      class="text-sky-500"
+      >Latest Release</a
+    >
+  </UContainer>
+</template>
